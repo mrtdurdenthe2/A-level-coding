6201b456415cc74b3acffc8a9b5bb75e5c624944
mrtdurdenthe2 <kai.wil.business@gmail.com>
2024-03-14 14:40:24 +0000

    Added breadth first search and new graph

--- graphsandtrees.py
+++ graphsandtrees.py
@@ -28,7 +28,7 @@ nodeB = mygraph.add_node(node='B'), mygraph.add_edge('B', 'C'), mygraph.add_edge
 nodeA = mygraph.add_node(node='A'), mygraph.add_edge('A', 'B'), mygraph.add_edge('A', 'D')
 nodeC = mygraph.add_node(node='C'), mygraph.add_edge('C', 'B'), mygraph.add_edge('C', 'D')
 nodeD = mygraph.add_node(node='D'), mygraph.add_edge('D', 'C'), mygraph.add_edge('D', 'A')
-#print(f"Final Graph:\n\n{mygraph.adjacency_list}")
+#5print(f"Final Graph:\n\n{mygraph.adjacency_list}")
 
 
 def depth_first_search_recursive(graph, current_node, visited=None):
@@ -41,16 +41,29 @@ def depth_first_search_recursive(graph, current_node, visited=None):
     for neighbor in graph[current_node]:
         if neighbor not in visited:
             depth_first_search_recursive(graph, neighbor, visited)
-graph = {
-    'A' : ['B','C','D'],
-    'B' : ['A', 'E', 'F'],
-    'C' : [],
-    'D' : ['A','G', 'H'],
-    'E' : ['B'],
-    'F' : ['B'],
-    'G' : ['B'],
-    'H' : ['B']
+graph = {'A' : ['B','C','D'],
+  'B' : ['A'],
+  'C' : ['A','E','F'],
+  'D' : ['A','F'],
+  'E' : ['C','F'],
+  'F' : ['C','D'],
 }
 
 
+def breadth_first_search(graph, start_node):
+    visited = set() 
+    queue = [start_node] 
+
+    while queue:
+        current_node = queue.pop(0)
+        
+        if current_node not in visited:
+            print(current_node)  
+            visited.add(current_node)
+
+            for neighbor in graph[current_node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+breadth_first_search(graph, 'A')
 depth_first_search_recursive(graph, 'A')
